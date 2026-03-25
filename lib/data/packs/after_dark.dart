93677cc3ee884d28e846d2a3460d5a97fa809972
Mario <mario.mandra@yahoo.com>
2026-03-25 03:26:30 +0200

    after dark v3

--- lib/data/packs/after_dark.dart
+++ lib/data/packs/after_dark.dart
@@ -4,74 +4,74 @@ import 'package:flutter/material.dart';
 
 const String afterDarkId = 'after_dark';
 const String afterDarkTitle = 'After Dark';
-const String afterDarkSubtitle = 'Late night party vibes. Awkward, fun, and parent-safe.';
+const String afterDarkSubtitle = 'Late night party vibes. Awkward, funny, and fast-paced.';
 const IconData afterDarkIcon = Icons.nightlight_round;
 const bool afterDarkIsLocked = true;
 const bool afterDarkRequiresPremium = false;
 
 const Map<String, List<String>> afterDarkWords = {
  'en': [
-    // Party & Drinks (20)
-    'Party', 'Club', 'DJ', 'Bartender', 'Bouncer', 'VIP', 'Karaoke', 'Dancefloor', 'Cocktail', 'Shot', 
-    'Tequila', 'Vodka', 'Beer', 'Wine', 'Champagne', 'Cheers', 'Hangover', 'Tipsy', 'Wasted', 'Blackout',
-    // Late Night Out (20)
-    'Happy Hour', 'Pub', 'Tavern', 'Brewery', 'Icebreaker', 'Confetti', 'Midnight', 'Taxi', 'Uber', 'Kebab', 
-    'Pizza', 'Fast Food', 'Cravings', 'Snack', 'Sleepover', 'Pyjamas', 'Afterparty', 'Playlist', 'Speaker', 'Microphone',
-    // Dating & Apps (20)
-    'Tinder', 'Match', 'Swipe', 'Profile', 'Bio', 'Selfie', 'Filter', 'DM', 'Seen', 'Ghosting', 
-    'Catfish', 'Red Flag', 'Green Flag', 'Date', 'Third Wheel', 'Wingman', 'Flirting', 'Wink', 'Crush', 'Friendzone',
-    // Romance & Drama (20)
-    'Ex-boyfriend', 'Ex-girlfriend', 'Single', 'Heartbreak', 'Jealousy', 'Serenade', 'Smooch', 'Cuddle', 'Snuggle', 'Romance', 
-    'Soulmate', 'Heartthrob', 'Chemistry', 'Rejection', 'Compliment', 'Sparks', 'Love', 'Kiss', 'Hug', 'Boyfriend',
-    // Social & Gossip (20)
-    'Gossip', 'Rumor', 'Drama', 'Paparazzi', 'Celebrity', 'Influencer', 'Viral', 'Trending', 'Troll', 'Hater', 
-    'Fake News', 'Hashtag', 'Follower', 'Subscriber', 'Unfollow', 'Block', 'Mute', 'Notification', 'Podcast', 'Vlogger',
-    // Confessions & Awkwardness (20)
-    'Embarrassment', 'Awkward', 'Cringe', 'Blushing', 'Apology', 'Excuse', 'Alibi', 'Lie', 'Truth', 'Dare', 
-    'Confession', 'Whisper', 'Promise', 'Betrayal', 'Revenge', 'Grudge', 'Forgiveness', 'Loyalty', 'Bestie', 'Frenemy',
-    // Late Night Binging (20)
-    'Binge-watching', 'Netflix', 'Streaming', 'Episode', 'Spoiler', 'Plot Twist', 'Finale', 'Cliffhanger', 'Cinema', 'Popcorn', 
-    'Ticket', 'Screen', 'Director', 'Actor', 'Actress', 'Red Carpet', 'Award', 'Trophy', 'Speech', 'Applause',
-    // Vices & Luck (20)
-    'Casino', 'Poker', 'Blackjack', 'Roulette', 'Jackpot', 'Bet', 'Gamble', 'Chips', 'Dealer', 'Bluff', 
-    'Lottery', 'Luck', 'Fortune', 'Superstition', 'Jinx', 'Karma', 'Destiny', 'Fate', 'Horoscope', 'Zodiac',
-    // Moods & Energy (20)
-    'Insomnia', 'Night Owl', 'Nightmare', 'Dream', 'Sleepwalking', 'Snoring', 'Yawn', 'Exhausted', 'Alarm', 'Snooze', 
-    'Caffeine', 'Coffee', 'Energy Drink', 'Espresso', 'Adrenaline', 'Panic', 'Chill', 'Relax', 'Massage', 'Spa',
-    // Night Themes & Tropes (20)
-    'Vampire', 'Bat', 'Owl', 'Moon', 'Stars', 'Galaxy', 'Telescope', 'Shadow', 'Silhouette', 'Ghost', 
-    'Mystery', 'Detective', 'Clue', 'Suspect', 'Witness', 'Guilty', 'Innocent', 'Handcuffs', 'Police', 'Siren'
+    // Drinks & Party (20)
+    'Beer', 'Wine', 'Vodka', 'Shot', 'Drunk', 'Tipsy', 'Hangover', 'Bar', 'Club', 'Bartender', 
+    'Bouncer', 'ID', 'Cocktail', 'Toast', 'Cheers', 'Sip', 'Thirst', 'Keg', 'Pub', 'Menu',
+    // Dating & Romance (20)
+    'Kiss', 'Hug', 'Date', 'Flirt', 'Crush', 'Ex', 'Single', 'Married', 'Divorce', 'Ring', 
+    'Wedding', 'Bride', 'Groom', 'Jealous', 'Cheating', 'Secret', 'Whisper', 'Wink', 'Smile', 'Blush',
+    // The Body & Bathroom (20) - Mild toilet humor is great for Alias
+    'Toilet', 'Poop', 'Pee', 'Fart', 'Burp', 'Sweat', 'Spit', 'Sneeze', 'Cough', 'Snore', 
+    'Naked', 'Shower', 'Bath', 'Towel', 'Soap', 'Toilet Paper', 'Belly', 'Butt', 'Lips', 'Tongue',
+    // Night & Sleep (20)
+    'Night', 'Dark', 'Sleep', 'Dream', 'Nightmare', 'Bed', 'Pillow', 'Blanket', 'Pajamas', 'Mattress', 
+    'Moon', 'Star', 'Midnight', 'Vampire', 'Ghost', 'Zombie', 'Monster', 'Bat', 'Owl', 'Shadow',
+    // Vices & Mischief (20)
+    'Smoke', 'Cigarette', 'Lighter', 'Ashtray', 'Casino', 'Bet', 'Cards', 'Poker', 'Money', 'Cash', 
+    'Thief', 'Steal', 'Police', 'Arrest', 'Jail', 'Handcuffs', 'Lie', 'Truth', 'Dare', 'Rule',
+    // Social & Loud (20)
+    'Party', 'Music', 'Dance', 'Song', 'DJ', 'Speaker', 'Microphone', 'Karaoke', 'Singer', 'Concert', 
+    'Crowd', 'VIP', 'Ticket', 'Invite', 'Guest', 'Host', 'Pizza', 'Snack', 'Trash', 'Mess',
+    // Modern Life (20)
+    'Phone', 'App', 'Swipe', 'Tinder', 'Text', 'Message', 'Call', 'Photo', 'Selfie', 'Video', 
+    'Screen', 'Password', 'Internet', 'Viral', 'Trend', 'Drama', 'Gossip', 'Rumor', 'Fake', 'News',
+    // Clothing (20)
+    'Bra', 'Underwear', 'Socks', 'Shoes', 'Heels', 'Dress', 'Skirt', 'Shirt', 'Pants', 'Belt', 
+    'Zipper', 'Button', 'Pocket', 'Hat', 'Glasses', 'Makeup', 'Lipstick', 'Perfume', 'Mirror', 'Comb',
+    // Emotions & Reactions (20)
+    'Cry', 'Laugh', 'Angry', 'Sad', 'Happy', 'Scared', 'Shock', 'Surprise', 'Bored', 'Tired', 
+    'Crazy', 'Stupid', 'Smart', 'Lazy', 'Fast', 'Slow', 'Loud', 'Quiet', 'Hot', 'Cold',
+    // Grown-up Basics (20)
+    'Boss', 'Job', 'Work', 'Office', 'Bill', 'Debt', 'Rent', 'Tax', 'Coffee', 'Traffic', 
+    'Car', 'Key', 'Door', 'Window', 'House', 'Neighbor', 'Doctor', 'Nurse', 'Sick', 'Pill'
   ],
   'ro': [
-    // Petrecere & Băuturi (20)
-    'Petrecere', 'Club', 'DJ', 'Barman', 'Bodyguard', 'VIP', 'Karaoke', 'Ring de dans', 'Cocktail', 'Shot', 
-    'Tequila', 'Vodcă', 'Bere', 'Vin', 'Șampanie', 'Noroc', 'Mahmureală', 'Amețit', 'Pilit', 'Rangă',
-    // Ieșiri Nocturne (20)
-    'Happy Hour', 'Pub', 'Tavernă', 'Berărie', 'Spărgător de gheață', 'Confeti', 'Miezul nopții', 'Taxi', 'Uber', 'Shaorma', 
-    'Pizza', 'Fast Food', 'Pofte', 'Gustare', 'Somn', 'Pijama', 'Afterparty', 'Playlist', 'Boxă', 'Microfon',
-    // Dating & Aplicații (20)
-    'Tinder', 'Match', 'Swipe', 'Profil', 'Bio', 'Selfie', 'Filtru', 'Mesaj', 'Seen', 'Ghosting', 
-    'Catfish', 'Steag roșu', 'Steag verde', 'Întâlnire', 'A treia roată', 'Complice', 'Flirt', 'Clipit', 'Pasiune', 'Friendzone',
-    // Romanță & Dramă (20)
-    'Fostul', 'Fosta', 'Singur', 'Inimă frântă', 'Gelozie', 'Serenadă', 'Pupic', 'Îmbrățișare', 'Alint', 'Romanță', 
-    'Suflet pereche', 'Cuceritor', 'Chimie', 'Refuz', 'Compliment', 'Scântei', 'Iubire', 'Sărut', 'Iubit', 'Iubită',
-    // Bârfă & Social Media (20)
-    'Bârfă', 'Zvon', 'Dramă', 'Paparazzi', 'Celebritate', 'Influencer', 'Viral', 'Trending', 'Troll', 'Hater', 
-    'Fake News', 'Hashtag', 'Urmăritor', 'Abonat', 'Unfollow', 'Block', 'Mute', 'Notificare', 'Podcast', 'Vlogger',
-    // Confesiuni & Situații Penibile (20)
-    'Rușine', 'Penibil', 'Cringe', 'Roșeață', 'Scuză', 'Pretext', 'Alibi', 'Minciună', 'Adevăr', 'Provocare', 
-    'Confesiune', 'Șoaptă', 'Promisiune', 'Trădare', 'Răzbunare', 'Pică', 'Iertare', 'Loialitate', 'BFF', 'Inamic',
-    // Binge-watching de Noapte (20)
-    'Maraton de seriale', 'Netflix', 'Streaming', 'Episod', 'Spoiler', 'Răsturnare de situație', 'Finală', 'Cliffhanger', 'Cinema', 'Floricele', 
-    'Bilet', 'Ecran', 'Regizor', 'Actor', 'Actriță', 'Covorul roșu', 'Premiu', 'Trofeu', 'Discurs', 'Aplauze',
-    // Vicii & Noroc (20)
-    'Cazinou', 'Poker', 'Blackjack', 'Ruletă', 'Jackpot', 'Pariu', 'Joc de noroc', 'Jetoane', 'Dealer', 'Cacealma', 
-    'Loterie', 'Noroc', 'Avere', 'Superstiție', 'Ghinion', 'Karma', 'Destin', 'Soartă', 'Horoscop', 'Zodiac',
-    // Stări & Energie (20)
-    'Insomnie', 'Pasăre de noapte', 'Coșmar', 'Vis', 'Somnambul', 'Sforăit', 'Căscat', 'Epuizat', 'Alarmă', 'Snooze', 
-    'Cofeină', 'Cafea', 'Energizant', 'Espresso', 'Adrenalină', 'Panică', 'Chill', 'Relaxare', 'Masaj', 'Spa',
-    // Tropi & Concepte Nocturne (20)
-    'Vampir', 'Liliac', 'Bufniță', 'Lună', 'Stele', 'Galaxie', 'Telescop', 'Umbră', 'Siluetă', 'Fantomă', 
-    'Mister', 'Detectiv', 'Indiciu', 'Suspect', 'Martor', 'Vinovat', 'Nevinovat', 'Cătușe', 'Poliție', 'Sirenă'
+    // Băuturi & Petrecere (20)
+    'Bere', 'Vin', 'Vodcă', 'Shot', 'Beat', 'Amețit', 'Mahmureală', 'Bar', 'Club', 'Barman', 
+    'Bodyguard', 'Buletin', 'Cocktail', 'Toast', 'Noroc', 'Înghițitură', 'Sete', 'Butoi', 'Pub', 'Meniu',
+    // Întâlniri & Romanță (20)
+    'Sărut', 'Îmbrățișare', 'Întâlnire', 'Flirt', 'Pasiune', 'Fostul', 'Singur', 'Căsătorit', 'Divorț', 'Inel', 
+    'Nuntă', 'Mireasă', 'Mire', 'Gelos', 'Înșelat', 'Secret', 'Șoaptă', 'Clipit', 'Zâmbet', 'Roșeață',
+    // Corp & Baie (20)
+    'Toaletă', 'Caca', 'Pișu', 'Pârț', 'Râgâit', 'Transpirație', 'Scuipat', 'Strănut', 'Tuse', 'Sforăit', 
+    'Dezbrăcat', 'Duș', 'Baie', 'Prosop', 'Săpun', 'Hârtie igienică', 'Burtă', 'Fund', 'Buze', 'Limbă',
+    // Noapte & Somn (20)
+    'Noapte', 'Întuneric', 'Somn', 'Vis', 'Coșmar', 'Pat', 'Pernă', 'Pătură', 'Pijama', 'Saltea', 
+    'Lună', 'Stea', 'Miezul nopții', 'Vampir', 'Fantomă', 'Zombi', 'Monstru', 'Liliac', 'Bufniță', 'Umbră',
+    // Vicii & Pozne (20)
+    'Fum', 'Țigară', 'Brichetă', 'Scrumieră', 'Cazinou', 'Pariu', 'Cărți', 'Poker', 'Bani', 'Cash', 
+    'Hoț', 'Furt', 'Poliție', 'Arest', 'Închisoare', 'Cătușe', 'Minciună', 'Adevăr', 'Provocare', 'Regulă',
+    // Socializare (20)
+    'Petrecere', 'Muzică', 'Dans', 'Cântec', 'DJ', 'Boxă', 'Microfon', 'Karaoke', 'Cântăreț', 'Concert', 
+    'Mulțime', 'VIP', 'Bilet', 'Invitație', 'Musafir', 'Gazdă', 'Pizza', 'Gustare', 'Gunoi', 'Dezordine',
+    // Viață Modernă (20)
+    'Telefon', 'Aplicație', 'Swipe', 'Tinder', 'Text', 'Mesaj', 'Apel', 'Poză', 'Selfie', 'Video', 
+    'Ecran', 'Parolă', 'Internet', 'Viral', 'Trend', 'Dramă', 'Bârfă', 'Zvon', 'Fals', 'Știri',
+    // Îmbrăcăminte (20)
+    'Sutien', 'Chiloți', 'Șosete', 'Pantofi', 'Tocuri', 'Rochie', 'Fustă', 'Cămașă', 'Pantaloni', 'Curea', 
+    'Fermoar', 'Nasture', 'Buzunar', 'Pălărie', 'Ochelari', 'Machiaj', 'Ruj', 'Parfum', 'Oglindă', 'Pieptene',
+    // Emoții & Reacții (20)
+    'Plâns', 'Râs', 'Supărat', 'Trist', 'Fericit', 'Speriat', 'Șoc', 'Surpriză', 'Plictisit', 'Obosit', 
+    'Nebun', 'Prost', 'Deștept', 'Leneș', 'Rapid', 'Încet', 'Gălăgios', 'Liniștit', 'Fierbinte', 'Rece',
+    // Chestii de "Oameni Mari" (20)
+    'Șef', 'Job', 'Muncă', 'Birou', 'Factură', 'Datorie', 'Chirie', 'Taxă', 'Cafea', 'Trafic', 
+    'Mașină', 'Cheie', 'Ușă', 'Fereastră', 'Casă', 'Vecin', 'Doctor', 'Asistentă', 'Bolnav', 'Pastilă'
   ],
 };
